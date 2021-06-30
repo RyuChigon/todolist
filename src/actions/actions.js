992110cb1bf4c8e63dc16ab33bab9691812af329
RyuChigon <fbclrhs10@gmail.com>
2021-06-30 15:51:51 +0900

    Fix: actions typo

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -11,25 +11,25 @@ export const writeTodo = (todo) => {
 export const selectTodo = (index) => {
   console.log('Action::SELECT_TODO');
   return {
-    types: types.SELECT_TODO,
+    type: types.SELECT_TODO,
     index
   };
 };
 
 export const unselectTodo = () => {
   console.log('Action::UNSELECT_TODO');
-  return {types: types.UNSELECT_TODO};
+  return {type: types.UNSELECT_TODO};
 };
 
 export const modifyTodo = (todo) => {
   console.log('ACtion::MODIFY_TODO');
   return {
-    types: types.MODIFY_TODO,
+    type: types.MODIFY_TODO,
     todo
   };
 };
 
 export const deleteTodo = () => {
   console.log('Action::DELETE_TODO');
-  return {types: types.DELETE_TODO};
+  return {type: types.DELETE_TODO};
 };
