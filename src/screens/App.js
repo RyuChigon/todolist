413609a514cd5a6300353e642dc212a82ea6731f
RyuChigon <fbclrhs10@gmail.com>
2021-06-30 15:56:32 +0900

    Fix: add Header in Router

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MainScreen from './MainScreen.js';
+import ViewScreen from './ViewScreen.js';
 import WriteScreen from './WriteScreen.js';
+import Header from '../components/Header/Header.js';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
+        <Header />
         <Route exact path="/" component={MainScreen} />
         <Route path="/write" component={WriteScreen} />
+        <Route path="/view" component={ViewScreen} />
       </Router>
     ) 
   }
