435ebd495b3ba760f9af456924a3ba2aae8267b8
RyuChigon <fbclrhs10@gmail.com>
2021-07-01 21:57:24 +0900

    Update Cache using redux-persist

--- src/index.js
+++ src/index.js
@@ -3,14 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './screens/App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
 
-const store = createStore(reducer);
+const persistConfig = {
+  key: 'app',
+  storage
+}
+
+const persisted = persistReducer(persistConfig, reducer);
+
+const store = createStore(persisted, compose(
+  applyMiddleware(promiseMiddleware, ReduxThunk),
+))
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store} >
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
   , document.getElementById('app')
 );
