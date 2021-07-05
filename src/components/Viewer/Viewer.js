6b8aca43651ad5cf7d397b81026d5f6002449903
RyuChigon <fbclrhs10@gmail.com>
2021-07-05 10:10:13 +0900

    Add Viewer.css

--- src/components/Viewer/Viewer.js
+++ src/components/Viewer/Viewer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SButton from '../SButton/SButton';
+import './Viewer.css';
 
 class Viewer extends React.Component {
   constructor(props) {
@@ -10,16 +11,28 @@ class Viewer extends React.Component {
   render() {
     return (
       <div> 
-        <Link to='/check'><SButton name='수정' onClick={() => this.props.onModify()} /></Link>
-        <Link to='/check'><SButton name='삭제' onClick={() => this.props.onDelete()} /></Link>
-        <p>작성자</p>
-        <p>{this.props.todo.writer}</p>
-        <p>제목</p>
-        <p>{this.props.todo.title}</p>
-        <p>내용</p>
+        <div className="mini">
+          <div className="mini_left" >
+            <p className="fix">제목</p>
+            <p className="title">{this.props.todo.title}</p>
+          </div>
+          <div className="mini_right">
+            <div className="mini_right_left">
+              <p className="fix">작성자</p>
+              <p className="writer">{this.props.todo.writer}</p>
+            </div>
+            <div className="mini_right_btn">
+              <Link to='/check'><SButton name='수정' onClick={() => this.props.onModify()} /></Link>
+              <Link to='/check'><SButton name='삭제' onClick={() => this.props.onDelete()} /></Link>
+              <Link to='/'><SButton onClick={() => this.props.onMain()} name='목록'/></Link>
+            </div>
+          </div>
+        </div>
+
+        <hr/>
         <p>{this.props.todo.content}</p>
-        <Link to='/'><SButton onClick={() => this.props.onMain()} name='목록'/></Link>
-        <p>댓글</p>
+        <hr/>
+        <p className="fix">댓글</p>
         <p>댓글 작성자</p>
         <input type="text" placeholder="댓글 작성자" value={this.props.nCommentWriter} onChange={(text) => this.props.changeNCommentWriter(text)} />
         <p>댓글 내용</p>
